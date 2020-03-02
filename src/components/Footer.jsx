8c18d5273ed1fb921d58a47fd0277a7e54e7b3ab
SpktrSyx <ballandras.cindy@gmail.com>
2020-03-02 16:14:07 +0100

    refactor + layout component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () =>
+  <footer>
+    Copyright test test
+  </footer>
+;
+
+export default Footer;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as ArticlePreview } from './ArticlePreview'
+export { default as Header } from './Header'
+export { default as Footer } from './Footer'
+export { default as ArticleList } from './ArticleList'
+export { default as Sidebar } from './Sidebar'
+export { default as Layout } from './Layout';
